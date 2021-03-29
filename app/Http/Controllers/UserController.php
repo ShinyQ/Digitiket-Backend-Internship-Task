20b4b5e1db8ba30cdf5c95d9132f8080044918d5
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2021-03-29 15:12:30 +0700

    adding basic model, controller, and migration for user and destination

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index(){
+
+    }
+
+    public function register(){
+
+    }
+
+    public function login(){
+
+    }
+
+    public function logout(){
+
+    }
+
+}
